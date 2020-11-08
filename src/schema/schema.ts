a1bceaf00427af2bd3e4cd0ed549366172e54059
vine1993 <vine.rangel93@gmail.com>
2020-11-08 13:31:49 -0300

    folder structure quick fixs

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 export const schema = makeSchema({
     types: [],
     outputs: {
-        typegen: join(__dirname,'..','nexus-typegen.ts'),
-        schema: join(__dirname,'..','schema.graphql')
+        typegen: join(__dirname,'../../','.nexus/nexus-typegen.ts'),
+        schema: join(__dirname,'../../','.nexus/schema.graphql')
     }
 })
