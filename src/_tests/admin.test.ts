45e41c4de796ef0a9e285f80ee2eb647a63d5249
vine1993 <vine.rangel93@gmail.com>
2020-11-10 21:19:25 -0300

    tdd - first sample implementation to tests

--- src/_tests/admin.test.ts
+++ src/_tests/admin.test.ts
@@ -3,7 +3,7 @@ import {createTestContext} from "./__helpers";
 const ctx = createTestContext();
 
 describe('Admin', () => {
-    it('should get the admin user', async ()=>{
+    it('should get no admin users', async ()=>{
         const result = await ctx.client.request(`
            query {
             Admins {
@@ -14,7 +14,18 @@ describe('Admin', () => {
             }
            } 
         `)
+        expect(result.Admins).toEqual(expect.arrayContaining([]));
+    })
 
-        expect(result.Admins).toBe(null);
+    it('should create a admin user', async () => {
+        const result = await ctx.client.request(`
+            mutation {
+                AdminCreate(name:"Vinicius Rangel",password:"123"){
+                    id
+                }
+            }
+        `)
+
+        expect(result.AdminCreate.id).toBe(1);
     })
 })
--- src/schemas/admin.ts
+++ src/schemas/admin.ts
@@ -1,4 +1,4 @@
-import { objectType,extendType } from "@nexus/schema";
+import {objectType, extendType, stringArg} from "@nexus/schema";
 import {query} from "express";
 
 export const Admin = objectType({
@@ -17,7 +17,28 @@ export const AdminQuery = extendType({
         t.field('Admins',{
             nullable:true,
             type:'Admin',
-            list:true
+            list:true,
+            resolve(_root,_args,ctx){
+                return [];
+            }
         })
     }
 })
+
+export const AdminMutation = extendType({
+    type: 'Mutation',
+    definition(t) {
+        t.field('AdminCreate',{
+            type: 'Admin',
+            nullable:false,
+            args: {
+              name: stringArg({required:true}),
+              password: stringArg({required:true})
+            },
+            resolve(_root,args,ctx){
+                return {id:1};
+            }
+        })
+    }
+})
+
